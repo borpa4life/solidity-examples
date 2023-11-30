abf81bd1ba2f62f2921c89058175180744e8c308
borpa4life <152425421+borpa4life@users.noreply.github.com>
2023-11-30 15:39:04 +0800

    add in operator role for tax

--- contracts/Borpa.sol
+++ contracts/Borpa.sol
@@ -5,13 +5,16 @@ import "./token/oft/v1/OFT.sol";
 
 contract Borpa is OFT {
     uint8 private _crossChainTaxRate;
+    // Operator is only allowed to modify tax rates
     address private _operator;
 
     constructor(address _layerZeroEndpoint) OFT("Borpa", "Borpa", _layerZeroEndpoint) {
         _mint(msg.sender, 100_000_000 * 1e18);
+        _operator = msg.sender;
     }
 
-    function setTax(uint8 _taxRate) public onlyOwner{
+    function setCrossChainTax(uint8 _taxRate) external{
+        require(msg.sender == _operator);
         require(_taxRate <= 20);
         _crossChainTaxRate = _taxRate;
     }
@@ -21,5 +24,10 @@ contract Borpa is OFT {
         _mint(owner(), tax);
         _mint(_toAddress, _amount - tax);
         return _amount;
+    }
+
+    function setOperator(address _newOperator) external{
+        require(msg.sender == _operator);
+        _operator = _newOperator;
     }
 }
